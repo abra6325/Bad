67e7e5ddbd0c27c86867a5f33b131586f37aded3
EricPooMan <63401790+EricPooMan@users.noreply.github.com>
2021-03-21 17:12:05 +0800

    Add files via upload

---
+++ salt-api.js
@@ -0,0 +1,28 @@
+function cset(name,value,expiredays,firstvisit){
+    var date = new Date();
+    date.setTime(date.getTime()+(expiredays*24*60*60*1000));
+    var expires = "expires="+date.toUTCString();
+    document.cookie=name+"="+value+";"+expiredays+";path=/";
+}
+function cget(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' '){
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function onLoad() {
+    var firstvisit = cget("firstvisit")
+    if (firstvisit == 0){
+        alert("newuser")
+    }
+}
+//from https://www.w3schools.com/js/js_cookies.asp thanks a lot
