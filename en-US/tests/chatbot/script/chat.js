5cf9e78d3bd5cc680efb7ee917301e5392c8d44f
EricPooMan <63401790+EricPooMan@users.noreply.github.com>
2021-03-21 10:35:44 +0800

    Add files via upload

--- en-US/tests/chatbot/script/chat.js
+++ en-US/tests/chatbot/script/chat.js
@@ -1,10 +1,16 @@
 //GET! SaltyFish.chatbot/index.html
 
 var sendbtn = document.getElementById("sendButton");
+var messages = ""
 
 // functions for the button pressing event
 function textSend(){
     var textarea = document.getElementById("chatinput");
+    var history = document.getElementById("history")
     // alert(textarea);
-    textarea.value = ""
+    if (textarea.value != ""){
+        messages += textarea.value + "<br>"
+        history.innerHTML = messages
+        textarea.value = ""
+    }
 }
