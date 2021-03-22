9376c85e24952ce48b24e987e6e30a61e554346e
EricPooMan <63401790+EricPooMan@users.noreply.github.com>
2021-03-22 20:03:43 +0800

    Add files via upload

---
+++ local-apps/salt-api.js
@@ -0,0 +1,38 @@
+function cset(name,value,expiredays){
+    var date = new Date();
+    date.setTime(date.getTime()+(expiredays*24*60*60*1000));
+    var expires = "expires="+date.toUTCString();
+    document.cookie=name+"="+value+";"+expiredays+";path=/";
+}
+function cget(cname){
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' '){
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function onLoad(){
+    var cookieagree = cget("cookieagree")
+    if (cookieagree != "true"){
+        cmessage()
+        cset("firstuser",1,999999)
+    }
+}
+function cmessage(){
+    var popup = document.getElementById("cookiemsg");
+    popup.classList.replace("idle","load");
+}
+function cagree(){
+    var popup = document.getElementById("cookiemsg");
+    popup.classList.replace("load","idle");
+    cset("cookieagree",true,999999)
+}
+//from https://www.w3schools.com/js/js_cookies.asp thanks a lot
