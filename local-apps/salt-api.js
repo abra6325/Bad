879258dc1cdf5ebdf8ebc68def83bca538839b00
EricPooMan <63401790+EricPooMan@users.noreply.github.com>
2021-08-06 21:11:58 +0800

    Add files via upload

--- local-apps/salt-api.js
+++ local-apps/salt-api.js
@@ -5,17 +5,23 @@ Any unauthorized used are prohibited.
 //Function Set / Variable
 console.log("%c %c %c %cSalty Fish API.main %c %c %c","background-color:#ebebeb","background-color:#bfbfbf","background-color:#969696","background-color:#969696;color:#ebebeb","background-color:#bfbfbf","background-color:#ebebeb","background-color:#ebebeb")
 
-var sq = document.getElementById("searchquery")
-var cb = document.getElementById("cookiebutton")
-var sb = document.getElementById("searchbutton")
-var lb = document.getElementById("loginb")
 
+
+function initl() {
+    var currentpage = window.location.href;
+    var http = new XMLHttpRequest();
+    http.open('HEAD', currentpage, false);
+    http.send();
+    if (http.status == 404){
+        console.error("%c %c %c %cCatch -> Error.404 on this page %c %c %c","background-color:#ffabab","background-color:#ff8585","background-color:#ff4f4f","background-color:#ff4f4f;color:#ebebeb","background-color:#ff8585","background-color:#ffabab","background-color:#ffabab")
+    }
+};
 function cset(name,value,expiredays){
     var date = new Date();
     date.setTime(date.getTime()+(expiredays*24*60*60*1000));
     var expires = "expires="+date.toUTCString();
     document.cookie=name+"="+value+";"+expiredays+";path=/";
-}
+};
 function cget(cname){
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
@@ -30,33 +36,27 @@ function cget(cname){
         }
     }
     return "";
-}
+};
 function onLoad(){
     var cookieagree = cget("cookieagree")
     if (cookieagree != "true"){
         cmessage()
         cset("firstuser",1,999999)
     }
-}
+};
 function page(){
     var titleElement = document.getElementsByTagName("title")[0]
     titleElement.innerHTML = pageData["pagetype"] + " | " + pageData["title"] + " | Salty Fish"
-}
+};
 function cmessage(){
     var popup = document.getElementById("cookiemsg");
     popup.classList.replace("idle","load");
-}
+};
 function cagree(){
     var popup = document.getElementById("cookiemsg");
     popup.classList.replace("load","idle");
     cset("cookieagree",true,999999)
-}
+};
 
-cb.onclick() = cagree();
-sb.onclick() = function(){
-    document.location.href = "/search?query=" + sq.value
-}
-lb.onclick() = function(){
-    document.location.href = "/login"
-}
+
 //some from https://www.w3schools.com/js/js_cookies.asp thanks a lot
