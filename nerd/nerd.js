df08ba60e78007c9c64c279d1b7fc06947e2388b
EricPooMan <63401790+EricPooMan@users.noreply.github.com>
2022-01-02 10:07:26 +0800

    Add files via upload

--- nerd/nerd.js
+++ nerd/nerd.js
@@ -1,1 +1,1 @@
-document.getElementById("invite").onclick=function(){window.location.href="https://discord.com/oauth2/authorize?client_id=898912199517556786&permissions=472446757968&scope=bot"}
+document.getElementById("invite").onclick=function(){window.location.href="https://discord.com/oauth2/authorize?client_id=898912199517556786&permissions=472446757968&scope=https://discord.com/api/oauth2/authorize?client_id=898912199517556786&permissions=475735223377&scope=bot%20applications.commands"}
